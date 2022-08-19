bb6441dc38e3582819696779bdabdedcd8477534
cb218 <cb218@students.uwf.edu>
2022-08-19 12:06:49 -0500

    modified parser.go for altered message format

--- parser.go
+++ parser.go
@@ -4,50 +4,56 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"time"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // Message The structs are defined from schema.json
 type Message struct {
-	Location	Location 	`json:"location"`
-	Reading		[]Reading 	`json:"reading"`
-	SensorType	string 		`json:"sensorType"`
-	Time		string 		`json:"time"`
-	Tower		string 		`json:"tower"`
+	Location    Location      `json:"location"`
+	Measurement []Measurement `json:"measurement"`
+	SensorType  string        `json:"sensorType"`
+	Time        string        `json:"time"`
+	Tower       string        `json:"tower"`
 }
-type Reading struct {
-	Measurement	string		`json:"measurement"`
-	Name		string		`json:"name"`
-	Value		string		`json:"value"`
+type Measurement struct {
+	Measurement string `json:"measurement"`
+	Name        string `json:"name"`
+	Value       string `json:"value"`
 }
 type Location struct {
-	Latitude	string		`json:"lat"`
-	Longitude	string		`json:"long"`
+	Latitude  string `json:"lat"`
+	Longitude string `json:"long"`
 }
 
 func Parse(file string) {
 	var message Message
 	data, err := ioutil.ReadFile(file)
-	if err != nil {fmt.Println(err)}
+	if err != nil {
+		fmt.Println(err)
+	}
 	err = json.Unmarshal(data, &message)
-	if err != nil {fmt.Println(err)}
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	tower := message.Tower
 	timeToken := strings.Split(message.Time, ":")
-	utime,errt := strconv.Atoi(timeToken[1])
-	if errt != nil {fmt.Println(errt)}
+	utime, errt := strconv.Atoi(timeToken[1])
+	if errt != nil {
+		fmt.Println(errt)
+	}
 	timestamp := time.Unix(int64(utime), 0)
 	var temperature string
-	for i := range message.Reading {
-		if message.Reading[i].Name == "temperature" {
-			temperature = message.Reading[i].Value
+	for i := range message.Measurement {
+		if message.Measurement[i].Name == "temperature" {
+			temperature = message.Measurement[i].Value
 		}
 	}
-	fmt.Printf("Tower: %s, Temperature: %s, Time %s",tower,temperature,timestamp)
+	fmt.Printf("Tower: %s, Temperature: %s, Time %s", tower, temperature, timestamp)
 }
 
 func main() {
-	Parse("example1.json")
+	Parse("example.json")
 }
